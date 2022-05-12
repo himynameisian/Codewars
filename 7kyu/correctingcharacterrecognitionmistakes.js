ef5c25dabf8290b4aa0271ca1dda9b8ad49d0302
himynameisian <iamian.kit@hotmail.com>
2022-05-12 16:47:38 -0400

    daily codewar added

---
+++ 7kyu/correctingcharacterrecognitionmistakes.js
@@ -0,0 +1,14 @@
+/*Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:*/
+
+function correct(string){
+	const correction = {
+    '5': 'S',
+    '0': 'O',
+    '1': 'I'
+  };
+  return string.split('').map((char) => correction.hasOwnProperty(char) ? correction[char] : char).join('')
+}
