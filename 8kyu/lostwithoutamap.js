723d299f97f3f244503be5e5085638c4673b5440
himynameisian <iamian.kit@hotmail.com>
2022-05-13 00:36:57 -0400

    went on a codewars crunch

---
+++ 8kyu/lostwithoutamap.js
@@ -0,0 +1,13 @@
+/*
+Beginner - Lost Without a Map
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2,4,6] */
+function maps(x){
+    let newArr = x.map(function(num){
+          return num * 2;
+      });
+    return newArr
+}
